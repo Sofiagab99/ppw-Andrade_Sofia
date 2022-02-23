bb31821cb0dc272ac49e3f17413fd217bdad3d67
Sofia <sofiagab99@gmail.com>
2022-02-22 23:55:09 -0500

    commit 6

--- products-api-company/src/app.js
+++ products-api-company/src/app.js
@@ -7,9 +7,11 @@ const app = express()
 
 
 
-app.set('pkg', pkg)
+app.set('pkg', pkg);
+
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.json({
--- products-api-company/src/controllers/products.controller.js
+++ products-api-company/src/controllers/products.controller.js
@@ -1,21 +1,34 @@
+import Product from "../models/Product"
 
-export const createProduct = (req, res) => {
+export const createProduct = async (req, res) => {
+    
 
-}
+    const{name, category, price, imgURL} = req.body
 
-export const getProducts = (req, res) => {
-    res.json("get Products")
+    const newProduct = new Product({name, category, price, imgURL});
 
-}
+    const productSaved = await newProduct.save()
 
-export const getProductById = (req, res) => {
-
-}
-
-export const updateProductById= (req, res) => {
+    res.status(201).json(productoSaved)
 
 }
 
-export const deleteProductById = (req, res) => {
+export const getProducts = async(req, res) => {
+    const products = await Product.find()
+    res.json(products)
+
+}
+
+export const getProductById = async(req, res) => {
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product)
+
+}
+
+export const updateProductById= async(req, res) => {
+
+}
+
+export const deleteProductById = async(req, res) => {
 
 }
