58111f0d222ed07cb9725b68c4a79e14626a9ffc
Sofia <sofiagab99@gmail.com>
2022-02-23 14:34:58 -0500

    commit 8

--- products-api-company/src/app.js
+++ products-api-company/src/app.js
@@ -2,11 +2,14 @@ import express, { json } from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
 
+import { createRoles } from './libs/initialSetup'
+
 import productsRoutes from './routes/products.routes'
 import authRoutes from './routes/auth.routes'
+import { createProduct } from './controllers/products.controller'
 
 const app = express()
-
+createRoles();
 
 
 app.set('pkg', pkg);
--- products-api-company/src/controllers/auth.controller.js
+++ products-api-company/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User'
 import jwt from 'jsonwebtoken'
 import config from '../config'
+import Role from '../models/Role';
 
 export const signup = async (req, res) => {
 
@@ -14,16 +15,34 @@ export const signup = async (req, res) => {
         password: await User.encryptPassword(password)
     })
 
+    if (roles) {
+        const foundRoles = await Role.find({name: {$in: roles }})
+        newUser.roles = foundRoles.map(role => role._id)
+    } else {
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id];
+    }
+    
+
    const savedUser =  await newUser.save()
+   console.log(savedUser)
    
-   jwt.sign({id: savedUser._id}, config.SECRET, {
+   const token = jwt.sign({id: savedUser._id}, config.SECRET, {
        expiraresIn: 86400
    })
+
    res.status.json({token})
 
 
 }
 
 export const signin = async (req, res) => {
-    res.json('signin')
+    
+    const userFound = await User.findOne({email: req.body.email}).populate()
+
+    if (!userFound) return res.status(400).json({message: "User not found"})
+
+    console.log(userFound)
+
+    res.json({token: ''})
 }
